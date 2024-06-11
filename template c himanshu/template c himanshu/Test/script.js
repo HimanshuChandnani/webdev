dfbffcdb9947d69d3a99d66b233573531bbc2c16
Rykt <138355402+HimanshuChandnani@users.noreply.github.com>
2024-06-11 20:07:18 +0530

    Add files via upload

---
+++ template c himanshu/template c himanshu/Test/script.js
@@ -0,0 +1,16 @@
+function active(elem){
+    document.getElementById('a1').classList.remove('active');
+    document.getElementById('a2').classList.remove('active');
+    document.getElementById('a3').classList.remove('active');
+    document.getElementById('a4').classList.remove('active');
+    document.getElementById('a5').classList.remove('active');
+    document.getElementById('a6').classList.remove('active');
+    document.getElementById('a7').classList.remove('active');
+    document.getElementById('a8').classList.remove('active');
+    elem.classList.add('active');
+    if(document.location.hash){
+        setTimeout(function() {
+            window.scrollTo(window.scrollX, window.scrollY - 69.4);
+        }, 500);
+    }
+}
